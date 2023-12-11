341f58aa690bb3308945118133b4ec84297898b2
syed <syed.hussain@oag.com>
2023-12-11 17:43:51 +0000

    Add fluent validation library

---
+++ Movies.Application/Validators/MovieValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Movies.Application.Models;
+
+namespace Movies.Application.Validators;
+
+public class MovieValidator : AbstractValidator<Movie>
+{
+    public MovieValidator()
+    {
+        RuleFor(movie => movie.Id).NotEmpty();//knows the types automatically does it
+        RuleFor(movie => movie.Genres).NotEmpty();//know its a count
+        RuleFor(movie => movie.Title).NotEmpty();//know its a count
+        
+        
+        
+    }
+}
