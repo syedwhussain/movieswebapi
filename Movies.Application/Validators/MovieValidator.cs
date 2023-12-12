0069e4eea128c84b63c7863840451a3aff38efa2
syed <syed.hussain@oag.com>
2023-12-12 13:00:18 +0000

    Add fluent validator with custom method for checking slug updated or not

--- Movies.Application/Validators/MovieValidator.cs
+++ Movies.Application/Validators/MovieValidator.cs
@@ -1,17 +1,47 @@
 ﻿using FluentValidation;
 using Movies.Application.Models;
+using Movies.Application.Services;
 
 namespace Movies.Application.Validators;
 
 public class MovieValidator : AbstractValidator<Movie>
 {
+    private readonly IMovieService _movieService;
     public MovieValidator()
     {
         RuleFor(movie => movie.Id).NotEmpty();//knows the types automatically does it
         RuleFor(movie => movie.Genres).NotEmpty();//know its a count
         RuleFor(movie => movie.Title).NotEmpty();//know its a count
+        //custom validation for YearOfRelease
+        RuleFor(m => m.YearOfRelease).GreaterThan(1940).LessThanOrEqualTo(DateTime.UtcNow.Year);
         
+        //add custom validation rule
         
-        
+        //on create it doesnt exist, on update it does....so if its update, it must be there.
+        RuleFor(m => m.Slug)
+            .MustAsync(ValidateSlug)
+            .WithMessage("This item already exists");
+
+    }
+
+    public async Task<bool> ValidateSlug(Movie movie, string slug, CancellationToken cancellationToken)
+    {
+        //get the movie based on the slug value first. this will coe from the presentation layer
+        var existingDbMovie = await _movieService.GetBySlugAsync(slug);
+
+        bool isValid = false;
+
+        //an existing item has this already, so we need to check if its the same item
+        if (existingDbMovie is not null && 
+                    existingDbMovie.Id == movie.Id)       {
+            isValid = true;
+        }
+
+        if(existingDbMovie is null)//in this casser its unique no other movie has this slug
+        {
+            isValid = true; //this is a create since we dont have it in the db. All good.
+        }
+
+        return isValid;
     }
 }
---
+++ Movies.Tests/ValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using Movies.Application.Models;
+using Movies.Application.Validators;
+
+namespace Movies.Tests;
+
+public class ValidatorTests
+{
+    [Fact]
+    public async Task show_run_validation_all_ok()
+    {
+        // Arrange
+        var movie = new Movie
+        {
+            // Initialize your movie object here
+            // For example:
+            Id = Guid.NewGuid(),
+            Title = "Test Title",
+            YearOfRelease = 2022,
+            Genres = new List<string> { "Test Genre" }
+        };
+
+        var movieValidator = new MovieValidator();
+
+        // Act
+        var validationResult = await movieValidator.ValidateAsync(movie);
+
+        // Assert
+        Assert.True(validationResult.IsValid);
+
+
+    }
+    
+    [Fact]
+    public async Task should_fail_with_year_too_high()
+    {
+        // Arrange
+        var movie = new Movie
+        {
+            // Initialize your movie object here
+            // For example:
+            Id = Guid.NewGuid(),
+            Title = "Test Title",
+            YearOfRelease = 2015,
+            Genres = new List<string> { "Test Genre" }
+        };
+
+        var movieValidator = new MovieValidator();
+
+        // Act
+        var validationResult = await movieValidator.ValidateAsync(movie);
+
+        // Assert
+        Assert.False(validationResult.IsValid,validationResult.Errors.ToString());
+
+
+    }
+}
